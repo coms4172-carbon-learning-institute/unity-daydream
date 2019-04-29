1b292fb9799f644317c351d1054296efb32e87c1
Lily He <yonglinlilyhe@gmail.com>
2019-04-28 21:05:28 -0400

    created basic minimap

---
+++ Assets/MinimapCameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject player;
+    public float minimapCameraOffset = 10.0f;
+    void Start()
+    {
+        transform.position = new Vector3(player.transform.position.x,player.transform.position.y +  minimapCameraOffset, player.transform.position.z);
+        transform.Rotate(90f, 0, 0);
+    }
+
+   
+}
