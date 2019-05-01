e8e2b33f13b596f6336d303990759eb84d7e863d
Wanlin Xie <wanlinx@gmail.com>
2019-05-01 14:06:22 -0400

    custom obj controller + info panel

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,45 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject[] selectedObjs;
+    public Material currentObjMaterial;
+    public GameObject infoPanel;
 
-  public GameObject[] selectedObjs;
-  public Material currentObjMaterial;
-  private Material hoverMaterial;
+    private Material hoverMaterial;
+    public GameObject currSelectedObj;
 
-  void Start()
-  {
-    hoverMaterial = Resources.Load("Materials/Hover") as Material;
-    selectedObjs = GameObject.FindGameObjectsWithTag("selectable");
+    void Start()
+    {
+        infoPanel.SetActive(false);
+        hoverMaterial = Resources.Load("Materials/Hover") as Material;
+        selectedObjs = GameObject.FindGameObjectsWithTag("selectable");
 
-    foreach (GameObject obj in selectedObjs) {
-        currentObjMaterial = obj.GetComponent<Renderer>().material;
-        obj.AddComponent<MeshCollider>();
-        obj.AddComponent<GoogleVR.HelloVR.ObjectController>();
+        foreach (GameObject obj in selectedObjs)
+        {
+            currentObjMaterial = obj.GetComponent<Renderer>().material;
+            obj.AddComponent<MeshCollider>();
+            //obj.AddComponent<GoogleVR.HelloVR.ObjectController>();
+            //obj.GetComponent<GoogleVR.HelloVR.ObjectController>().inactiveMaterial = currentObjMaterial;
+            //obj.GetComponent<GoogleVR.HelloVR.ObjectController>().gazedAtMaterial = hoverMaterial;
+            obj.AddComponent<CustomObjectController>();
+            obj.GetComponent<CustomObjectController>().inactiveMaterial = currentObjMaterial;
+            obj.GetComponent<CustomObjectController>().gazedAtMaterial = hoverMaterial;
 
-        obj.GetComponent<GoogleVR.HelloVR.ObjectController>().inactiveMaterial = currentObjMaterial;
-        obj.GetComponent<GoogleVR.HelloVR.ObjectController>().gazedAtMaterial = hoverMaterial;
+            //pointerEnter
 
-        //pointerEnter
-
-        EventTrigger eventTrigger = obj.AddComponent<EventTrigger>();
-        EventTrigger.Entry entry1 = new EventTrigger.Entry();
-        entry1.eventID = EventTriggerType.PointerEnter;
-        //eventTrigger.triggers.Add(entry1);
-        entry1.callback.AddListener((eventData) => obj.GetComponent<GoogleVR.HelloVR.ObjectController>().SetGazedAt(true));
-        eventTrigger.triggers.Add(entry1);
-        //print(entry1.);
+            EventTrigger eventTrigger = obj.AddComponent<EventTrigger>();
+            EventTrigger.Entry entry1 = new EventTrigger.Entry();
+            entry1.eventID = EventTriggerType.PointerEnter;
+            //entry1.callback.AddListener((eventData) => obj.GetComponent<GoogleVR.HelloVR.ObjectController>().SetGazedAt(true));
+            entry1.callback.AddListener((eventData) => obj.GetComponent<CustomObjectController>().SetGazedAt(true));
+            eventTrigger.triggers.Add(entry1);
 
 
-        //pointerExit
-        EventTrigger.Entry entry2 = new EventTrigger.Entry();
-        entry2.eventID = EventTriggerType.PointerExit;
-        
-        entry2.callback.AddListener((eventData) => obj.GetComponent<GoogleVR.HelloVR.ObjectController>().SetGazedAt(false));
-        eventTrigger.triggers.Add(entry2);
+            //pointerExit
+            EventTrigger.Entry entry2 = new EventTrigger.Entry();
+            entry2.eventID = EventTriggerType.PointerExit;
+            //entry2.callback.AddListener((eventData) => obj.GetComponent<GoogleVR.HelloVR.ObjectController>().SetGazedAt(false));
+            entry2.callback.AddListener((eventData) => obj.GetComponent<CustomObjectController>().SetGazedAt(false));
+            eventTrigger.triggers.Add(entry2);
+        }
     }
-  }
+
+    private void Update()
+    {
+        currSelectedObj = EventSystem.current.currentSelectedGameObject;
+        if (currSelectedObj != null)
+        {
+            print("SELECTED");
+            infoPanel.SetActive(true);
+        }
+        else
+        {
+            infoPanel.SetActive(false);
+        }
+    }
 }
---
+++ Assets/Resources/Scripts/CustomObjectController.cs
@@ -0,0 +1,68 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ObjectController.cs" company="Google Inc.">
+// Copyright 2014 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//-----------------------------------------------------------------------
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomObjectController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material inactiveMaterial;
+
+   
+    public Material gazedAtMaterial;
+
+    private Vector3 startingPosition;
+    private Renderer myRenderer;
+
+    /// <summary>Sets this instance's GazedAt state.</summary>
+    /// <param name="gazedAt">
+    /// Value `true` if this object is being gazed at, `false` otherwise.
+    /// </param>
+    public void SetGazedAt(bool gazedAt)
+    {
+        if (inactiveMaterial != null && gazedAtMaterial != null)
+        {
+            myRenderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+            return;
+        }
+    }
+
+    /// <summary>Resets this instance and its siblings to their starting positions.</summary>
+    public void Reset()
+    {
+        int sibIdx = transform.GetSiblingIndex();
+        int numSibs = transform.parent.childCount;
+        for (int i = 0; i < numSibs; i++)
+        {
+            GameObject sib = transform.parent.GetChild(i).gameObject;
+            sib.transform.localPosition = startingPosition;
+            sib.SetActive(i == sibIdx);
+        }
+    }
+
+
+    private void Start()
+    {
+        startingPosition = transform.localPosition;
+        myRenderer = GetComponent<Renderer>();
+        SetGazedAt(false);
+    }
+}
