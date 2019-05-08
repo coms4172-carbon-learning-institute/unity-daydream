183a1e13ca778320b1475ac625638ae7ce26e6b5
racello <g.plus.wan@gmail.com>
2019-05-08 13:34:15 -0400

    welcome screen with simple script

---
+++ Assets/WelcomeScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WelcomeScreen : MonoBehaviour
+{
+    public Button start;
+    public GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(true);
+        start.onClick.AddListener(Begin);
+    }
+
+    void Begin()
+    {
+        panel.SetActive(false);
+    }
+}
