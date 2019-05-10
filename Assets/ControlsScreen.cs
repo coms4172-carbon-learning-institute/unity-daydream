a6360ff27b5f9a22119841553a38f171c77e9a1a
racello <g.plus.wan@gmail.com>
2019-05-09 23:20:41 -0400

    added control instructions screen

---
+++ Assets/ControlsScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlsScreen : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject welcome;
+    public Button HowToUse;
+    public Button close;
+
+    void Start()
+    {
+        panel.SetActive(false);
+        HowToUse.onClick.AddListener(TurnOn);
+        close.onClick.AddListener(TurnOff);
+    }
+
+    void TurnOn()
+    {
+        panel.SetActive(true);
+        welcome.SetActive(false);
+    }
+
+    void TurnOff()
+    {
+        panel.SetActive(false);
+    }
+}
