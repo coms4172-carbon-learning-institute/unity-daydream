253df6580d92b71e34d43a6d0776902a19ae0a80
Supriya Khandekar <supriyaskhandekar@gmail.com>
2019-05-10 19:23:54 -0400

    Working on rotation and panel

---
+++ Assets/ButtonManipulationController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManipulationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool translate;
+    public bool transform;
+    public bool rotate;
+    public Button translateBtn;
+    public Button transformBtn;
+    public Button rotateBtn;
+
+    void Start()
+    {
+        translate = false;
+        transform = false;
+        rotate = false;
+    }
+
+    // Update is called once per frame
+
+    public void onClick() {
+
+      if (gameObject == translateBtn) {
+
+        translate = true;
+        transform = false;
+        rotate = false;
+
+      } else if (gameObject == transformBtn) {
+
+        transform = true;
+        translate = false;
+        rotate = false;
+
+      } else {
+
+        rotate = true;
+        transform = false;
+        translate = false;
+      }
+
+    }
+
+    void Update()
+    {
+
+    }
+}
--- Assets/Manipulation.cs
+++ Assets/Manipulation.cs
@@ -24,33 +24,7 @@ public class Manipulation : MonoBehaviour
       //gameObject.AddListener(EventTriggerType.PointerUp, Release);
     }
 
-    public void Hold() {
-    // get the Transform component of the pointer
-        Transform pointerTransform = GvrPointerInputModule.Pointer.PointerTransform;
-        Debug.Log(pointerTransform);
-        // set the GameObject's parent to the pointer
-        transform.SetParent(pointerTransform, false);
-        // position it in the view
-        //transform.localPosition = new Vector3(0, 0, 2);
 
-        // disable physics
-        //GetComponent<Rigidbody>().isKinematic = true;
-    }
-
-    /*
-    public void Translate() {
-
-      Debug.Log("in translate");
-      Debug.Log(pointer.gameObject.transform.position);
-    }
-
-    */
-
-    public void Move() {
-      Debug.Log("in drag function");
-      this.gameObject.transform.position = pointer.gameObject.transform.position;
-
-    }
 
     public void SetGazedAt(bool gazedAt)
     {
@@ -63,41 +37,34 @@ public class Manipulation : MonoBehaviour
         }
     }
 
-    /*
+
+    public void Rotate() {
+
+      Transform pointerTransform = GvrPointerInputModule.Pointer.PointerTransform;
+
+
+
+    }
+
     public void Release() {
-  // set the parent to the world
+
+
+
+
+    }
+    public void Hold() {
+
+        Transform pointerTransform = GvrPointerInputModule.Pointer.PointerTransform;
+        transform.SetParent(pointerTransform, false);
+    }
+
+    public void Release() {
+
       transform.SetParent(null, true);
-      // get the rigidbody physics component
-      Rigidbody rigidbody = GetComponent<Rigidbody>();
-      // reset velocity
-      rigidbody.velocity = Vector3.zero;
-      // enable physics
-      rigidbody.isKinematic = false;
-    }
-    */
-
-    public void testFct() {
-
-        Debug.Log("hello");
-        Debug.Log("dragging");
 
     }
 
 
-
-    /*
-    public void Translate(PointerEventData data) {
-
-      Debug.Log(data);
-    }
-    /*
-
-    public void OnDrag(PointerEventData data) {
-
-        Debug.Log("hello");
-
-    }
-    */
 
     /*
     public void Translate() {
