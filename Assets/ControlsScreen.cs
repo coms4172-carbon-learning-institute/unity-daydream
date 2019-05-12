9cf71a176717ef911e6ac19c33b77980b456fedb
Lily He <yonglinlilyhe@gmail.com>
2019-05-11 21:14:45 -0400

    increase size of buttons

--- Assets/ControlsScreen.cs
+++ Assets/ControlsScreen.cs
@@ -8,23 +8,16 @@ public class ControlsScreen : MonoBehaviour
     public GameObject panel;
     public GameObject welcome;
     public Button HowToUse;
-    public Button close;
 
     void Start()
     {
         panel.SetActive(false);
         HowToUse.onClick.AddListener(TurnOn);
-        close.onClick.AddListener(TurnOff);
     }
 
     void TurnOn()
     {
         panel.SetActive(true);
         welcome.SetActive(false);
-    }
-
-    void TurnOff()
-    {
-        panel.SetActive(false);
     }
 }
